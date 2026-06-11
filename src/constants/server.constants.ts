a7af0d13d2553ff5cd455beeef3a422e2d1c8892
sabaaa1 <sdeli23@freeuni.edu.ge>
2026-06-12 00:01:14 +0400

    fix relayer url

--- src/constants/server.constants.ts
+++ src/constants/server.constants.ts
@@ -6,6 +6,7 @@ export const API_BASE_URL = ENCLAVE_API_PRODUCTION_URL;
 
 export const RELAYER_URL_LOCAL = "http://localhost:7073";
 export const RELAYER_STAGING_URL = "https://wallet-staging.hinkal.io/relayer";
-export const RELAYER_PRODUCTION_URL = "https://wallet-prod.hinkal.io/relayer";
+export const RELAYER_PRODUCTION_URL =
+  "https://wallet-prodv12.hinkal.io/relayer";
 
 export const RELAYER_BASE_URL = RELAYER_PRODUCTION_URL;
